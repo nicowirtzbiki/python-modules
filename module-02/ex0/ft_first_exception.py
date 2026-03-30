0cba41a12929dafafe7264e5f3691d911128bcf4
nicowirtzbiki <wirtz.nico@gmail.com>
2026-03-30 08:36:27 +0100

    ex0 completed

--- module-02/ex0/ft_first_exception.py
+++ module-02/ex0/ft_first_exception.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+def input_temperature(temp_str: str) -> int:
+    temp = int(temp_str)
+    return temp
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    print()
+    try:
+        print("Input data is '25'")
+        temp = input_temperature("25")
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print()
+    try:
+        print("Input data is 'abc'")
+        temp = input_temperature("abc")
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    print()
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    test_temperature()
