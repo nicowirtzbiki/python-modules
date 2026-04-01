55716e15979357fb2a878b8d5e86e3aa62cfa3ea
nicowirtzbiki <wirtz.nico@gmail.com>
2026-04-01 22:36:14 +0100

    ex4 completed. flake8 and mypy OK.

--- module-02/ex4/ft_finally_block.py
+++ module-02/ex4/ft_finally_block.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown garden error") -> None:
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown plant error") -> None:
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message: str = "Unknown water error") -> None:
+        super().__init__(message)
+
+
+def water_plant(plant_name: str) -> None:
+    if plant_name == plant_name.capitalize():
+        print(f"Watering {plant_name}: [OK]")
+    else:
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+
+
+def test_watering_system() -> None:
+    print("=== Garden Watering System ===")
+    print()
+    print("Testing valid plants...")
+    try:
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("Lettuce")
+        water_plant("Carrots")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        print(".. ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+    print()
+    print("Testing invalid plants...")
+    try:
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("lettuce")  # ← inválido!
+        water_plant("Carrots")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        print(".. ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+        print()
+        print("Cleanup always happens, even with errors!")
+
+
+if __name__ == "__main__":
+    test_watering_system()
