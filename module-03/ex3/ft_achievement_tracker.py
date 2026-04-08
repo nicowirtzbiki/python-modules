8a42ebf0ace2476afa169d5a22a9a76ac37e52a0
Nicole Wirtzbiki <nwirtzbi@c2r3s4.42porto.com>
2026-04-08 22:06:14 +0100

    new gitignore and ex3 still unfinished

--- module-03/ex3/ft_achievement_tracker.py
+++ module-03/ex3/ft_achievement_tracker.py
@@ -2,7 +2,7 @@
 import random
 
 def gen_player_achievements() -> set[str]:
-    players = ["Alice", "Bob", "Charlie", "Dylan"]
+    
     achievements = ['Crafting Genius', 'Strategist', 
                     'World Savior', 'Speed Runner', 
                     'Survivor', 'Master Explorer', 
@@ -10,4 +10,35 @@ def gen_player_achievements() -> set[str]:
                     'First Steps', 'Collector Supreme', 
                     ' Untouchable', 'Sharp Mind', 
                     'Boss Slayer']
-    nbr_achievements = random.randint(0, len(achievements))
+    nbr_achievements = random.randint(1, len(achievements))
+    selected = random.sample(achievements, k=nbr_achievements)
+    return set(selected)
+
+
+def main() -> None:
+    players = ["Alice", "Bob", "Charlie", "Dylan"]
+    achievements = []
+    for player in players:
+        achievements.append(gen_player_achievements())
+
+    print("=== Achievement Tracker System ===")
+    print()
+    for i in range (len(players)):
+        print(f"Player {players[i]}: {achievements[i]}")
+    print()
+    unique_achievements = set()
+    common_achievements = achievement[0]
+    different_achievements = set()
+    for achievement in achievements:
+        unique_achievements = unique_achievements.union(achievement)
+    for achievement in achievements[1:]:
+        common_achievements = common_achievements.intersection(achievement)
+    different_achievements = different_achievements.difference(achievement)
+    print(f"All distinct achievements: {unique_achievements}")
+    print()
+    print(f"Common achievements: {common_achievements}")
+    print()
+    print()
+
+if __name__ == "__main__":
+    main()
